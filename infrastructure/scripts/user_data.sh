4e368239e45bef0c4a01c73345ed0909f68e801e
Nagy Gergő <gergosculpture@gmail.com>
2025-06-06 13:21:14 +0200

    fix(user_data): fix installing runner

--- infrastructure/scripts/user_data.sh
+++ infrastructure/scripts/user_data.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y docker awscli
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
 
-mkdir /actions-runner && cd /actions-runner || exit
-curl -o actions-runner-linux-x64-2.317.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz
-tar xzf ./actions-runner-linux-x64-2.317.0.tar.gz
+sudo yum install -y curl unzip jq
 
-sudo ./config.sh --url ${repo_url} --token ${github_token} --name "aws-runner-$(hostname)" --work _work --labels aws-runner --unattended
+if ! command -v aws &> /dev/null; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+fi
 
-sudo ./svc.sh install
-sudo ./svc.sh start
+mkdir -p /home/ec2-user/actions-runner && cd /home/ec2-user/actions-runner
+curl -o actions-runner-linux-x64.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz
+tar xzf actions-runner-linux-x64.tar.gz
+chown -R ec2-user:ec2-user /home/ec2-user/actions-runner
+
+sudo su - ec2-user -c "cd ~/actions-runner && ./config.sh --url ${repo_url} --token ${github_token} --labels aws-runner --name github-runner"
+sudo su - ec2-user -c "cd ~/actions-runner && ./run.sh &"
+
+sudo chmod 644 /var/log/amazon/ssm/amazon-ssm-agent.log
